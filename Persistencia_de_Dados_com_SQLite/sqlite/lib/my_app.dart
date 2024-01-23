ae052e807e37124b6beb3087bd23410677ed92d4
jedson <jhoni.palmera5@gmail.com>
2024-01-23 05:13:08 -0300

    Usando o banco de dados sqflite para armazenar as tarefas

---
+++ Persistencia_de_Dados_com_SQLite/sqlite/lib/my_app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'pages/login_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      theme: ThemeData(
+          useMaterial3: false,
+          primarySwatch: Colors.blue,
+          textTheme: GoogleFonts.robotoTextTheme()),
+      home: const LoginPage(),
+    );
+  }
+}
---
+++ Persistencia_de_Dados_com_SQLite/sqlite/lib/pages/main_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:trilhaapp/pages/card_page.dart';
+import 'package:trilhaapp/pages/image_assets.dart.dart';
+import 'package:trilhaapp/pages/list_view.dart.dart';
+import 'package:trilhaapp/pages/tarefa_page/tarefa_sqlite_page.dart';
+import 'package:trilhaapp/shared/widgets/custom_drawer.dart';
+import 'list_view_horizontal.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  PageController controller = PageController(initialPage: 0);
+  int posicaoPagina = 0;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Main Page"),
+        ),
+        drawer: const CustonDrawer(),
+        body: Column(
+          children: [
+            Expanded(
+              child: PageView(
+                controller: controller,
+                onPageChanged: (value) {
+                  setState(() {
+                    posicaoPagina = value;
+                  });
+                },
+                children: const [
+                  CardPage(),
+                  ImageAssetsPage(),
+                  ListViewPage(),
+                  ListViewHorizontal(),
+                  TarefaSQLitePage()
+                ],
+              ),
+            ),
+            BottomNavigationBar(
+                type: BottomNavigationBarType.fixed,
+                onTap: (value) {
+                  controller.jumpToPage(value);
+                },
+                currentIndex: posicaoPagina,
+                items: const [
+                  BottomNavigationBarItem(
+                      label: "Pag1", icon: Icon(Icons.home)),
+                  BottomNavigationBarItem(label: "Pag2", icon: Icon(Icons.add)),
+                  BottomNavigationBarItem(
+                      label: "Pag3", icon: Icon(Icons.person)),
+                  BottomNavigationBarItem(
+                      label: "Pag4", icon: Icon(Icons.image)),
+                  BottomNavigationBarItem(
+                      label: "Tarefas", icon: Icon(Icons.list))
+                ])
+          ],
+        ),
+      ),
+    );
+  }
+}
